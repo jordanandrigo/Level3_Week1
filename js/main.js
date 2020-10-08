4d81d00a1809602359313ed1b5a5c73c8c59bf5b
Jordan Andrigo <jordanandrigo@Jordans-MacBook-Pro.local>
2020-10-08 14:17:38 -0400

    adding updated files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 // import your packages here
-import Person from "./modules/DataModule.js";
+import Team from "./modules/DataModule.js";
 
 (() => {
     // stub * just a place for non-component-specific stuff
-    console.log('loaded');
+    let userSection = document.querySelector(".users-section"),
+        userTemplate = document.querySelector("#profs-template").content;
 
-    console.log(Person);
+    debugger;
+
+    // select our user elements and load the content
+
+    function handleDataSet(data) {
+        for (let user in data) {
+
+            //make a copy of our template here and then populate the cildren (text elements) 
+            // with the static data from the Team object
+
+                 let currentUser = userTemplate.cloneNode(true),
+                    currentUserText = currentUser.querySelector('.user').children;
+
+       
+                 currentUserText[1].textContent = Team[user].name;
+                 currentUserText[2].textContent = Team[user].role;
+                 currentUserText[3].textContent = Team[user].nickname;
+
+                 userSection.appendChild(currentUser);
+            }
+
+
+    }
+
+    handleDataSet();
+
 })();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,25 @@
 export default {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Trevor"
+    Jordan: {
+        name: "Jordan",
+        role: "Student",
+        nickname: "Jord"
+    },
+
+    Sarah: {
+        name: "Sarah",
+        role: "Main Homie",
+        nickname: "Gerald"
+    },
+
+    Ted: {
+        name: "Ted",
+        role: "Cat/Pet",
+        nickname: "T E D"
+    },
+
+    Tessa: {
+        name: "Tessa",
+        role: "Master Developer",
+        nickname: "T-Coops"
+    }
 }
